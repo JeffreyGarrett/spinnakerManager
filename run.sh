427b772f4585b13d98c2ee633cb4184aa2b3b3d4
= <jeff2invest@gmail.com>
2021-01-11 07:32:15 -0800

    adding all

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 # Builds Docker image and enters a terminal session inside the container.
 # Allows AWSCLI and Terraform to be run consistently between users and scripts.
 
-docker build -t spin-manager . && docker run -it --rm -v "/$(PWD)":/spin_manager:Z -w /spin_manager -v "/$HOME/.aws":/root/.aws spin-manager sh
+docker build -t spin-manager . && docker run -it --rm -v "/$(PWD)":/spin_manager:Z -w /spin_manager/cli -v "/$HOME/.aws":/root/.aws spin-manager sh
